d312aa13a8e3ddf94835dc1cfc57f25e59dd9c3a
JP Erasmus <jperasmus11@gmail.com>
2018-08-31 13:17:37 +0200

    chore: Starting point

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span className="App-logo">🐢</span>
+          <h1 className="App-title">Perril Terril FTW!</h1>
+        </header>
+        <p className="App-intro">Let's gooi 🌽</p>
+      </div>
+    );
+  }
+}
+
+export default App;
